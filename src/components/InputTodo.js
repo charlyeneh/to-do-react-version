6c769a42b87b9b74cdd36b6d0f74eb0eca27febb
Emperor_charly <charleschukwunweikeeneh@gmail.com>
2022-03-24 10:59:35 +0100

    Add inputTodu.js file

---
+++ src/components/InputTodo.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react"
+
+class InputTodo extends Component {
+  state = {
+  title: ""
+  };
+  
+onChange = e => {
+  this.setState({
+    [e.target.name]: e.target.value,
+  });
+};
+  
+  handleSubmit = e => {
+    e.preventDefault();
+      console.log(this.state.title);
+      this.props.addTodoProps(this.state.title);
+      this.setState({
+      title: ""
+    });
+  };
+  
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="Add Todo..." value={this.state.title} onChange={this.onChange} name="title" />
+        <button>Submit</button>
+      </form>
+    )
+  }
+}
+export default InputTodo
