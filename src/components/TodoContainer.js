73a863f551fe2c4b67db8672398dacf05a91d51d
Emperor_charly <charleschukwunweikeeneh@gmail.com>
2022-03-24 11:00:59 +0100

    Update the TodoItem.js file

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,6 +1,7 @@
 import React from "react"
 import TodosList from "./TodosList";
 import Header from "./Header";
+import InputTodo from "./InputTodo"
 
 class TodoContainer extends React.Component {
 state = {
@@ -8,12 +9,12 @@ state = {
    {
      id: 1,
      title: "Setup development environment",
-     completed: true
+     completed: false
    },
    {
      id: 2,
      title: "Develop website and add content",
-     completed: false
+     completed: true
    },
    {
      id: 3,
@@ -22,13 +23,58 @@ state = {
    }
   ]
 };
+  
+//Handler for the checkbox
+handleChange = id => {
+  this.setState(prevState => {
+  return {
+    todos: prevState.todos.map(todo => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completed: !todo.completed,
+        }
+      }
+      return todo
+    }),
+  }
+  })
+};
+
+//Handle the delete
+delTodo = id => {
+  this.setState({
+    todos: [
+      ...this.state.todos.filter(todo => {
+        return todo.id !== id;
+      })
+    ]
+  });
+};
+
+  addTodoItem = title => {
+    const newTodo = {
+    id: 4,
+    title: title,
+    completed: false
+    };
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    });
+  };
+  
   render() {
     return (
       <div>
-        <Header/>
-        <TodosList todos={this.state.todos} />  
+        <Header />
+        <InputTodo addTodoProps={this.addTodoItem} />
+        <TodosList todos={this.state.todos}
+          handleChangeProps={this.handleChange}
+          deleteTodoProps={this.delTodo}
+        />
       </div>
     )
   }
 }
+
 export default TodoContainer
